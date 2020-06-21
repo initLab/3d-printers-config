1dc9b57672bb09267ce3e1a43227c732a729e1af
Vencislav Atanasov <user890104@users.noreply.github.com>
2020-06-21 21:24:05 +0300

    Add nginx config, add script to fix C200 cam

---
+++ streamer/snapshot-i3mega.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Logitech C200 camera produces JPEG files without Huffman tables
+URL=http://127.0.0.1:8081/snapshot.jpeg
+FILENAME=/tmp/snapshot-i3mega.jpeg
+
+# Comment these two to enable debugging
+CURL_OPT=-s
+JPEGOPTIM_QUIET=-q
+
+# Verbose cURL
+#CURL_OPT=-v
+
+while [ 1 ]
+do
+	curl $CURL_OPT -f -o $FILENAME $URL && \
+	jpegoptim $JPEGOPTIM_QUIET $FILENAME
+	sleep 1
+done
